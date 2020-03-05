012cfd056743205798c81f6bb885c1b39a2108f3
Serhii <serhii.londar@gmail.com>
2020-03-05 02:09:02 +0200

    Update podsfiles

--- Tests/Pods/Target Support Files/CrowdinSDK/CrowdinSDK-Unit-CrowdinSDK_Tests-frameworks.sh
+++ Tests/Pods/Target Support Files/CrowdinSDK/CrowdinSDK-Unit-CrowdinSDK_Tests-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CrowdinSDK/CrowdinSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BaseAPI/BaseAPI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CrowdinSDK/CrowdinSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BaseAPI/BaseAPI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
