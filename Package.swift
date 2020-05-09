25a609f0d45f71a724d71017d7affda9ab411dd5
Denys Meloshyn <dmel@nuuday.com>
2020-05-09 20:15:03 +0200

    products

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "mobile-sdk-ios",
-    dependencies: [
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "mobile-sdk-ios",
+            targets: ["mobile-sdk-ios"]),
+    ], dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
