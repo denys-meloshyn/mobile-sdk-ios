508e3b1aee5860f1c0b42931d9fac2f3568b23ab
Denys Meloshyn <dmel@nuuday.com>
2020-05-17 20:57:57 +0200

    test

--- Package.swift
+++ Package.swift
@@ -8,12 +8,12 @@ let package = Package(
     platforms: [
         .iOS(.v12),
     ],
-//    products: [
-//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-//        .library(
-//            name: "CrowdinSDK",
-//            targets: ["CrowdinSDK"]),
-//    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CrowdinSDK",
+            targets: ["CrowdinSDK"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
